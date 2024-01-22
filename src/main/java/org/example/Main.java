473203e5f65473f4a84270d6d520460a115f5d82
Egor Popov <ijfgio@yandex.ru>
2024-01-22 14:31:58 +0300

    testTwo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-      int count = WeekendsService.calculate(10000, 3000, 20000);
-        System.out.println("Сколько месяцев можно отдохнуть в год "+ count);
+        int count = WeekendsService.calculate(10000, 3000, 20000);
+        System.out.println("Сколько месяцев можно отдохнуть в год " + count);
     }
 }
--- src/test/java/org/example/Pacet/WeekendServiceTest.java
+++ src/test/java/org/example/Pacet/WeekendServiceTest.java
@@ -1,13 +1,22 @@
+package org.example.Pacet;
+
 import org.example.WeekendsService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class WeekendServiceTest {
     @Test
-            public void shouldCalculateWeekendService1() {
+    public void shouldCalculateWeekendService1() {
         int count = WeekendsService.calculate(10000, 3000, 20000);
         int expected = 3;
 
+        Assertions.assertEquals(expected, count);
+    }
+
+    @Test
+    public void shouldCalculateWeekendService2() {
+        int count = WeekendsService.calculate(100000, 60000, 150000);
+        int expected = 2;
         Assertions.assertEquals(expected, count);
     }
 }
